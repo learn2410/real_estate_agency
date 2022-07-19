5fc2bb8cf87d03f44dd8e376079c5ef05e898aed
learn2410 <learn2410@mail.ru>
2022-07-19 23:08:03 +0500

    fill Owner.flats, remove owner fileds from Flat

--- property/admin.py
+++ property/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from .models import Flat, Conplaint,Owner
+from .models import Flat, Conplaint, Owner
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'owner', 'address',)
+    search_fields = ('town', 'address')
     readonly_fields = ('created_at',)
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town','owners_phonenumber','owner_pure_phone')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
     list_editable = ['new_building']
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('liked_by',)
@@ -15,8 +15,10 @@ class FlatAdmin(admin.ModelAdmin):
 class ConplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('problem_flat',)
 
+
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
+
 
 admin.site.register(Flat, FlatAdmin)
 admin.site.register(Conplaint, ConplaintAdmin)
